e944c097a54c767edb6dc52fbe5fc655a0f03aea
Justin Cremer <jacremer@live.com>
2021-12-26 23:07:05 -0800

    makes kernal_main look nicer

--- src/kernel.c
+++ src/kernel.c
@@ -5,23 +5,26 @@
 #include "std/print.h"
 
 void kernel_init() {
+  terminal_init();
+  println("Initialize VGA adapter: [ok]");
   // Initialize kernel heap
   kheap_init();
+  println("Initialize kernel heap: [ok]");
   // Initialize interrupt descriptor table
   idt_init();
-  // Initialize VGA terminal
-  terminal_init();
+  println("Initialize interrupt descriptor table: [ok]");
+
+  println("Kernel successfully loaded\n");
 }
 
 void kernel_main() {
   kernel_init();
+
+  println("Hello world");
 
   void *ptr = kmalloc(50);
   void *ptr_2 = kmalloc(1000);
 
   kfree(ptr);
   kfree(ptr_2);
-
-  println("Hello world");
-  println("This should be on a new line");
 }
